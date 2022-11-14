5783d10006a708531dc38bb8f2a6c967d910e598
baku2426@gmail.com <baku2426@gmail.com>
2022-11-14 20:49:11 +0100

    Save

--- philos/remove.c
+++ philos/remove.c
@@ -1,12 +1,5 @@
 #include "philos.h"
 
-static t_philo	*first_philo(t_philo *philo)
-{
-	while (philo->n != 1)
-		philo = philo->next;
-	return (philo);
-}
-
 static void	remove_one(t_philo *philo)
 {
 	if (&philo->fork)
@@ -22,27 +15,13 @@ void	remove_all(t_philo *philo)
 	t_philo	*iter;
 	int		number_of_philos;
 
-	if (philo->config.number_of_philos == 1)
-		return (remove_one(philo));
-	iter = first_philo(philo)->next;
-	philo = iter->prev;
-	if (&philo->fork)
-		pthread_mutex_destroy(&philo->fork);
-	pthread_join(philo->thread, NULL);
-	if (philo)
-		free(philo);
-	philo = NULL;
-	philo = iter;
-	number_of_philos = philo->config.number_of_philos;
-	while (--number_of_philos)
+	number_of_philos = philo->config.number_of_philos + 1;
+	while (--number_of_philos && philo)
 	{
-		iter = philo->next;
-		if (&philo->fork)
-			pthread_mutex_destroy(&philo->fork);
-		pthread_join(philo->thread, NULL);
-		if (philo)
-			free(philo);
-		philo = NULL;
-		philo = iter;
+		if (philo->next)
+			iter = philo->next;
+		remove_one(philo);
+		if (iter)
+			philo = iter;
 	}
 }
